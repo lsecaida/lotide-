f12653d58bdb2a0438995608b55d228cc0c75f3a
Luis Secaida <luissecaida@gmail.com>
2023-03-24 22:38:37 -0400

    Uploading this file with the template literal syntax and also using different code

--- assertEqual.js
+++ assertEqual.js
@@ -1,18 +1,19 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected);
-  console.log("Assertion Passed: [" + actual + "] " + "===" + " [" + expected + "]");
- 
-  if (actual !== expected) {
-    console.log("Assertion Failed: [" + actual + "] " + "!==" + " [" + expected + "]");
+  
+  if (actual === expected) {
+    console.log(`Assertion Passed: [${actual}] === [${expected}]`);
+    
+  } else {
+    
+  } {
+    console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
 
   }
-
-};
-
-
-
+}
+ 
+  
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual("Tomatoe", "Pineapple");
-assertEqual(7, 7);
+assertEqual(7, 8);
