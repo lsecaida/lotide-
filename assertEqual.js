cbf88b02a84a6427bf3ae618292af6c8f5f215d3
Luis Secaida <luissecaida@gmail.com>
2023-03-24 21:13:19 -0400

    Adding another school file

---
+++ assertEqual.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected);
+  console.log("Assertion Passed: [" + actual + "] " + "===" + " [" + expected + "]");
+ 
+  if (actual !== expected) {
+    console.log("Assertion Failed: [" + actual + "] " + "!==" + " [" + expected + "]");
+
+  }
+
+};
+
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Tomatoe", "Pineapple");
+assertEqual(7, 7);
