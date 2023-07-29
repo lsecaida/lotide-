e25ce0f278a2e7650f41b86dd8a00c4e32d0b71d
Luis Secaida <124916500+lsecaida@users.noreply.github.com>
2023-07-29 07:46:19 -0400

    Add files via upload

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1}  === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1}  !== ${array2}`);
+  }
+};
+
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) return false;
+  else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+//TEST CODE
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // Pass
+assertArraysEqual(["1", "2", "3"], ["1", "2", "4"]); // Fail
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3", "4"]); // Fail
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // Fail
