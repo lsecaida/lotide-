260841c7b10a323d9e1b10f4563bdd55fb07b227
Luis Secaida <124916500+lsecaida@users.noreply.github.com>
2023-07-30 08:12:54 -0400

    Add files via upload

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+const findKeyByValue = function (object, value) {
+  const objectKeys = Object.keys(object);
+
+  for (let key of objectKeys) {
+    console.log(key);
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
