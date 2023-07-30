5d286371173ef5a308fbc7af74972e7d1ddb56e7
Luis Secaida <124916500+lsecaida@users.noreply.github.com>
2023-07-29 21:20:31 -0400

    Add files via upload

---
+++ letterPositions.js
@@ -0,0 +1,35 @@
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1}  === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1}  !== ${array2}`);
+  }
+};
+
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) return false;
+  else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const letterPositions = function (sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] === " ") {
+      continue;
+    } else if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions("Lighthouse in the house"));
