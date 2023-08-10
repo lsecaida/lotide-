aba340b9411b135fc4905137c45c614eadda37b1
Luis Secaida <124916500+lsecaida@users.noreply.github.com>
2023-08-10 11:05:40 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const eqObjects = require("./eqObjects");
+const findKey = require("./findKey");
+const head = require("./head");
+const assertArraysEqual = require("./assertArraysEqual");
+const middle = require("./middle");
+const eqArrays = require("./eqArrays");
+const assertEqual = require("./assertEqual");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const countLetters = require("./countLetters");
+const takeUntil = require("./takeUntil");
+const countOnly = require("./countOnly");
+const flatten = require("./flatten");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+const findKeyByValue = require("./findKeyByValue");
+const without = require("./without");
+module.exports = {
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
+};
