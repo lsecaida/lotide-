aab45689d206e1c6ef39d6a5132a606ec5b486b3
Luis Secaida <124916500+lsecaida@users.noreply.github.com>
2023-08-10 21:05:28 -0400

    Add files via upload

--- test/headTest.js
+++ test/headTest.js
@@ -1,17 +1,21 @@
-// const assertEqual = require("../assertEqual");
-// const head = require("../head");
-
-// // assertEqual(head([5, 6, 7]), 5);
-// // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-const assert = require("chai").assert;
 const head = require("../head");
+const assert = require("chai").assert;
+//const assertEqual = require('../assertEqual');
 
 describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
-  });
-
-  it("returns '5' for ['5']", () => {
-    assert.strictEqual(head(["5"]), "5");
+  }),
+    it("returns '5' for ['5']", () => {
+      assert.strictEqual(head(["5"]), "5");
+    }),
+    it("returns '5' for [5,6,7]", () => {
+      assert.strictEqual(head([5, 6, 7]), 5);
+    }),
+    it("returns 'Labs' for ['Labs', 'Lighthouse', 'Labs']", () => {
+      assert.strictEqual(head(["Labs", "Lighthouse", "Labs"]), "Labs");
+    });
+  it("returns 'you' for ['you',6,'hi']", () => {
+    assert.strictEqual(head(["you", 6, "hi"]), "you");
   });
 });
